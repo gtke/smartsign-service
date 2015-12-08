d0433ae321936058f16c803d9491f4afc2731c86
darrienj <daBlackSaiyan27@gmail.com>
2015-12-07 21:13:25 -0500

    Update ButtonOverlay.java
    More comments.  All da comments!  Commenting ButtonOverlay, explaining why this otherwise useless class is still around.

--- app/src/main/java/com/example/smartsign/smartsignservice/ButtonOverlay.java
+++ app/src/main/java/com/example/smartsign/smartsignservice/ButtonOverlay.java
@@ -7,11 +7,14 @@ import android.widget.Toast;
 
 /**
  * Created by drichmond on 10/16/15.
+ * This class represents the button overlay functionality where an overlay button is placed over the entire android UI
+ * no matter what app is currently running.  This functionality has been removed.
+ * This class is only being kept because it represents previously needed functionality.  Being kept for historical purposes
+ * in case it is determined that the buttonOverlay functionality is advantageous to this app at a later point in time.
  */
 public class ButtonOverlay extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Go to the settings page.
-        Toast.makeText(getApplicationContext(), "Overlay Activity", Toast.LENGTH_SHORT).show();
     }
 }
